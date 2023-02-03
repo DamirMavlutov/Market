e6c47a2469cf4e5c570ec42adbec03787e4b0af3
Damir <DM936420@yandex.ru>
2023-02-03 12:02:33 +0700

    done Card and Cards components

---
+++ src/components/2shop/Cards.jsx
@@ -0,0 +1,27 @@
+import Card from "./Card";
+import { useState, useEffect } from "react";
+
+const Cards = () => {
+  const [items, setItems] = useState("");
+  useEffect(() => {
+    fetch("https://63db6d43a3ac95cec5a16e6c.mockapi.io/api/v1/cloth")
+      .then((response) => response.json())
+      .then((json) => setItems(json));
+  }, []);
+  console.log(items);
+
+  return (
+    <>
+      {items.map((person) => {
+        return (
+          <Card
+            key={person.id}
+            {...person}
+          />
+        );
+      })}
+    </>
+  );
+};
+
+export default Cards;
