2e76f54b83375c11772c9c14b0ede96fce26b036
Damir <DM936420@yandex.ru>
2023-02-07 10:42:16 +0700

    DetailPage: size, color buttons

--- src/components/3detail/DetailPage.jsx
+++ src/components/3detail/DetailPage.jsx
@@ -1,6 +1,7 @@
 import { Link, useParams } from "react-router-dom";
 import React, { useState, useEffect } from "react";
-import styles from "./Unvisible.module.css";
+import RadioButton from "./RadioButton";
+
 const DEFAULT_PHOTO =
   "https://images.unsplash.com/photo-1672310035534-7f48cfb23164?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80";
 const DetailPage = () => {
@@ -105,77 +106,12 @@ const DetailPage = () => {
               <div className="d-flex mb-3">
                 <p className="text-dark font-weight-medium mb-0 mr-3">Sizes:</p>
                 <form>
-                  <div className="custom-control custom-radio custom-control-inline">
-                    <input
-                      type="radio"
-                      className="custom-control-input"
-                      id="m"
-                      name="size"
+                  {data.sizes.map((el) => (
+                    <RadioButton
+                      id={el}
+                      label={el.toUpperCase()}
                     />
-                    <label
-                      className="custom-control-label"
-                      for="size-1"
-                    >
-                      XS
-                    </label>
-                  </div>
-
-                  <div className="custom-control custom-radio custom-control-inline">
-                    <input
-                      type="radio"
-                      className="custom-control-input"
-                      id="size-2"
-                      name="size"
-                    />
-                    <label
-                      className="custom-control-label"
-                      for="size-2"
-                    >
-                      S
-                    </label>
-                  </div>
-                  <div className="custom-control custom-radio custom-control-inline">
-                    <input
-                      type="radio"
-                      className="custom-control-input"
-                      id="size-3"
-                      name="size"
-                    />
-                    <label
-                      className="custom-control-label"
-                      for="size-3"
-                    >
-                      M
-                    </label>
-                  </div>
-                  <div className="custom-control custom-radio custom-control-inline">
-                    <input
-                      type="radio"
-                      className="custom-control-input"
-                      id="size-4"
-                      name="size"
-                    />
-                    <label
-                      className="custom-control-label"
-                      for="size-4"
-                    >
-                      L
-                    </label>
-                  </div>
-                  <div className="custom-control custom-radio custom-control-inline">
-                    <input
-                      type="radio"
-                      className="custom-control-input"
-                      id="size-5"
-                      name="size"
-                    />
-                    <label
-                      className="custom-control-label"
-                      for="size-5"
-                    >
-                      XL
-                    </label>
-                  </div>
+                  ))}
                 </form>
               </div>
               <div className="d-flex mb-4">
@@ -183,76 +119,12 @@ const DetailPage = () => {
                   Colors:
                 </p>
                 <form>
-                  <div className="custom-control custom-radio custom-control-inline">
-                    <input
-                      type="radio"
-                      className="custom-control-input"
-                      id="color-1"
-                      name="color"
+                  {data.colors.map((el) => (
+                    <RadioButton
+                      id={el}
+                      label={el[0].toUpperCase() + el.substring(1)}
                     />
-                    <label
-                      className="custom-control-label"
-                      for="color-1"
-                    >
-                      Black
-                    </label>
-                  </div>
-                  <div className="custom-control custom-radio custom-control-inline">
-                    <input
-                      type="radio"
-                      className="custom-control-input"
-                      id="color-2"
-                      name="color"
-                    />
-                    <label
-                      className="custom-control-label"
-                      for="color-2"
-                    >
-                      White
-                    </label>
-                  </div>
-                  <div className="custom-control custom-radio custom-control-inline">
-                    <input
-                      type="radio"
-                      className="custom-control-input"
-                      id="color-3"
-                      name="color"
-                    />
-                    <label
-                      className="custom-control-label"
-                      for="color-3"
-                    >
-                      Red
-                    </label>
-                  </div>
-                  <div className="custom-control custom-radio custom-control-inline">
-                    <input
-                      type="radio"
-                      className="custom-control-input"
-                      id="color-4"
-                      name="color"
-                    />
-                    <label
-                      className="custom-control-label"
-                      for="color-4"
-                    >
-                      Blue
-                    </label>
-                  </div>
-                  <div className="custom-control custom-radio custom-control-inline">
-                    <input
-                      type="radio"
-                      className="custom-control-input"
-                      id="color-5"
-                      name="color"
-                    />
-                    <label
-                      className="custom-control-label"
-                      for="color-5"
-                    >
-                      Green
-                    </label>
-                  </div>
+                  ))}
                 </form>
               </div>
               <div className="d-flex align-items-center mb-4 pt-2">
