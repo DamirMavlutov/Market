5619fbfb40da1d8f9e174d831e1af7f897028e5f
Damir <DM936420@yandex.ru>
2023-03-23 16:35:21 +0700

    filter

--- src/components/2shop/Cards.jsx
+++ src/components/2shop/Cards.jsx
@@ -8,7 +8,6 @@ const Cards = () => {
   const sizes = useSelector((state) => state.dataReduser.sizes)
     .filter((el) => el.checked === true) //фильтруем все checked:true
     .map((el) => el.name); //делаем массив из содержимого name
-
   const colors = useSelector((state) => state.dataReduser.colors)
     .filter((el) => el.checked === true)
     .map((el) => el.name);
@@ -21,6 +20,7 @@ const Cards = () => {
     const { dataReduser } = state;
     return dataReduser.data;
   });
+
   useEffect(() => {
     fetch("https://63db6d43a3ac95cec5a16e6c.mockapi.io/api/v1/cloth")
       .then((response) => response.json())
@@ -29,29 +29,33 @@ const Cards = () => {
 
   const filteredItems = [];
   items.forEach((item) => {
-    const found = item.sizes.some((r) => sizes.includes(r)); //смотрим содержит ли item элем. sizes
-    if (found) {
-      filteredItems.push(item);
-    }
-  });
+    const isSizes = item.sizes.some((r) => sizes.includes(r)); //смотрим содержит ли item элем. sizes
+    const isColors = item.colors.some((r) => colors.includes(r)); //смотрим содержит ли item элем. colors
+    let dataPrices = +item.price;
+    let isPrices = false;
 
-  items.forEach((item) => {
-    const found = item.colors.some((r) => colors.includes(r)); //смотрим содержит ли item элем. colors
-
-    if (found) {
-      filteredItems.push(item);
-    }
-  });
-
-  items.forEach((item) => {
-    let num = +item.price;
     for (let i = 0; i < prices.length; i++) {
-      if (num >= prices[i].from && num <= prices[i].to) {
-        filteredItems.push(item);
+      if (dataPrices >= prices[i].from && dataPrices <= prices[i].to) {
+        isPrices = true;
         break;
       }
     }
+
+    if (isSizes || isColors || isPrices) {
+      filteredItems.push(item);
+    }
   });
+
+  if (
+    (sizes.length || colors.length || prices.length) &&
+    !filteredItems.length
+  ) {
+    return <div>Empty</div>;
+  }
+
+  if (!filteredItems.length) {
+    filteredItems.push(...items);
+  }
 
   return (
     <>
